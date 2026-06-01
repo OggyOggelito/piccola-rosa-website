791b47623d9e6bd481e6b628660eb734ba265ec9
Oscar Gullberg <oscar.ogg1999@gmail.com>
2026-06-01 16:15:23 +0200

    fixing issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Nuova Piccola Rosa",
+  title: "Nya Piccola Rosa",
   description: "Italiensk restaurang i Hässelby Strand",
 };
 
@@ -26,14 +26,12 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="sv"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-screen flex flex-col bg-white text-black overflow-x-hidden">
         <Navbar />
-        <main className="flex-1 w-full overflow-x-hidden">
-          {children}
-        </main>
+        <main className="flex-1 w-full overflow-x-hidden">{children}</main>
         <Footer />
       </body>
     </html>
